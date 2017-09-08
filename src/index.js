b2dc92ca2a0074d388bf8696b518c816b645903f
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-09-08 12:18:51 +0545

    not required now

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
- <div>Remainder Pro</div>, document.getElementById('root')
+ <div className="container"><h2 className="text-success">Hello World, this is react!</h2></div>, document.getElementById('root')
 )
